e94347e1a82e3b95f92ae7ba01e5efcf48a45a05
Manal Arora <manalarora21@gmail.com>
2021-10-17 21:49:53 +0530

    build

---
+++ precache-manifest.6ab46076fa7fdef36ea50b24fcbd3e03.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae22ad8a393079ffbdc8cdb0937c386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "718ba2f31c42eb1d9e5f",
+    "url": "/static/css/3.b71c89e3.chunk.css"
+  },
+  {
+    "revision": "37d843ae875893924329",
+    "url": "/static/css/4.9673d624.chunk.css"
+  },
+  {
+    "revision": "7e2c3cef5d7c7f005b5d",
+    "url": "/static/css/main.076776d5.chunk.css"
+  },
+  {
+    "revision": "47d9fdd40352deed5494",
+    "url": "/static/js/2.1580e227.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.1580e227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718ba2f31c42eb1d9e5f",
+    "url": "/static/js/3.33d6f3dd.chunk.js"
+  },
+  {
+    "revision": "37d843ae875893924329",
+    "url": "/static/js/4.2279d5c3.chunk.js"
+  },
+  {
+    "revision": "7e2c3cef5d7c7f005b5d",
+    "url": "/static/js/main.97b71792.chunk.js"
+  },
+  {
+    "revision": "66d8373a71e20b037628",
+    "url": "/static/js/runtime-main.96b35c44.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "0dff32880b17ff323d0e7ec6059d5354",
+    "url": "/static/media/Goldman_Sachs_Logo.0dff3288.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "bc8fcb0fca60814f6c459bab8c2dff0e",
+    "url": "/static/media/developerActivity.bc8fcb0f.svg"
+  },
+  {
+    "revision": "f383d927ec989fdf0aa304364a968c5c",
+    "url": "/static/media/developerWithHeadphones.f383d927.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "8cf714ca20c24f3d2dc9f34ae60569be",
+    "url": "/static/media/iiitDelhiLogo.8cf714ca.jpeg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "6eba3f6cbb0dfda3ad88fdfcee6bea42",
+    "url": "/static/media/nsit_logo.6eba3f6c.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "aa9761f1912da7f016f92a4386eb4cc2",
+    "url": "/static/media/springdales_logo.aa9761f1.png"
+  },
+  {
+    "revision": "7d03d48f69186a31e19d905ef10454fc",
+    "url": "/static/media/talksCardBack.7d03d48f.svg"
+  },
+  {
+    "revision": "00f73a5177b5eec99d8cbac67c012993",
+    "url": "/static/media/workingOnCouch.00f73a51.svg"
+  }
+]);
